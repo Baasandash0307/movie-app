c26db0ee429a06027ba94b10de76ca8aee48ffcc
Baasandash0307 <bsskaa.kai123@gmail.com>
2025-03-10 23:20:31 +0800

    new

---
+++ src/app/components/slideshow.tsx
@@ -0,0 +1,11 @@
+
+const Slideshow = () => {
+    return (
+      <div className="pt-[24px]">
+        <img className="w-full h-full" src="slide1.jpg"></img>
+      </div>
+    );
+  };
+  
+  export default Slideshow;
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/header"
+import Slideshow from "./components/slideshow"
 
 export default function Home() {
   return (
     <div>
       <Header></Header>
+      <Slideshow></Slideshow>
     </div>
   );
 }
