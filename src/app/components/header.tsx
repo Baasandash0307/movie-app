d6cd43ad16e91818281d9003538a48bf7c1e5cf6
24LU4966 24LU4966 <bsskaa.kai123@gmail.com>
2025-03-12 12:32:42 +0800

    new

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -18,31 +18,31 @@ import {
 
 const Header = () => {
   return (
-    <div className="w-full h-[59px] flex justify-around items-center">
-      <div className="w-[92px] h=[20px] flex justify-center items-center gap-2">
-        <img className="w-5 h-5" src="film.png"></img>
-        <p className="text-indigo-700 text-[16px] font-bold">Movie Z</p>
-      </div>
-      <div className="w-[438px] h-[36px] flex justify-center gap-3 ">
-        <Select>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Genre" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="light">Light</SelectItem>
-            <SelectItem value="dark">Dark</SelectItem>
-            <SelectItem value="system">System</SelectItem>
-          </SelectContent>
-        </Select>
-        <div className="flex justify-between items-center gap-3 border-1 rounded-[8px] pl-4 border-gray-300">
-          <img className="w-[16px] h-4" src="magnifier.png"></img>
-          <input className="w-[379px] h-[36px] outline-0" placeholder="Search" type="search"></input>
+      <div className="w-[1280px] h-[59px] flex justify-around items-center">
+        <div className="w-[92px] h-[20px] flex justify-center items-center gap-2">
+          <img className="w-5 h-5" src="film.png"></img>
+          <p className="text-indigo-700 text-[16px] font-bold">Movie Z</p>
+        </div>
+        <div className="w-[438px] h-[36px] flex justify-center gap-3 ">
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Genre" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Light</SelectItem>
+              <SelectItem value="dark">Dark</SelectItem>
+              <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
+          <div className="flex justify-between items-center gap-3 border-1 rounded-[8px] pl-4 border-gray-300">
+            <img className="w-[16px] h-4" src="magnifier.png"></img>
+            <input className="w-[379px] h-[36px] outline-0" placeholder="Search" type="search"></input>
+          </div>
+        </div>
+        <div className="w-[36px] h-[36px] border-[1px] flex justify-center items-center rounded-xl border-gray-300">
+          <button className="cursor-pointer"><img className="w-[16px] h-[16px]" src="moon.png"></img></button>
         </div>
       </div>
-      <div className="w-[36px] h-[36px] border-[1px] flex justify-center items-center rounded-xl border-gray-300">
-        <button className="cursor-pointer"><img className="w-[16px] h-[16px]" src="moon.png"></img></button>
-      </div>
-    </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,21 +11,21 @@ export default function Home() {
   const [movieList, setMovieList] = useState([])
 
 
-  // const options = {
-  //   method: "GET",
-  //   url: "https://api.themoviedb.org/3/discover/movie",
-  //   params: {
-  //     include_adult: "false",
-  //     include_video: "false",
-  //     language: "en-US",
-  //     page: "1",
-  //     sort_by: "popularity.desc",
-  //   },
-  //   headers: {
-  //     accept: "application/json",
-  //     Authorization: `Bearer ${ACCESS_TOKEN}`,
-  //   },
-  // };
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/discover/movie",
+    params: {
+      include_adult: "false",
+      include_video: "false",
+      language: "en-US",
+      page: "1",
+      sort_by: "popularity.desc",
+    },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    },
+  };
   const getMovies = async () => {
     const movies = await axios.get(
       "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}",
@@ -44,17 +44,32 @@ export default function Home() {
   }, [])
 
   return (
-    <div>
-      <Header></Header>
-      <Slideshow></Slideshow>
-      {movieList.map((movie) => (
-        <div>
-          <h1>{movie.title}</h1>
-          {movie.poster_path && (
-            <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={`${movie.title} Poster`}></img>
-          )}
+    <div className="w-full">
+      <div>
+        <div className="flex justify-center flex-wrap">
+          <Header></Header>
+          <Slideshow></Slideshow>
+          <div className="flex justify-between w-full">
+          <h1 className="text-[24px] font-bold">Upcoming</h1>
+          <div className="flex items-center gap-1">
+            <button>See More</button>
+            <img className="w-4 h-4" src="arrow-right.png"></img>
+          </div>
         </div>
-      ))}
+        </div>
+
+
+        <div className="flex flex-wrap justify-center">
+          {movieList.splice(0, 10).map((movie) => (
+            <div>
+              {movie.poster_path && (
+                <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={`${movie.title} Poster`}></img>
+              )}
+              <h1>{movie.title}</h1>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
